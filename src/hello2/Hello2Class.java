76934e5f79262a415a3d8b004bbe53b78a7dec4c
조재웅 <@DESKTOP-P39DUQ7>
2021-11-16 17:31:39 +0900

    헬로 월드

--- src/hello2/Hello2Class.java
+++ src/hello2/Hello2Class.java
@@ -14,10 +14,11 @@ public class Hello2Class {
 		// 나는 누구입니다
 		// 잘 부탁드립니다
 		String b ="1회"; // 문자열 변수 선언
+		String c = "2회";
 		a = 0; // 위에 미리 선언됨 중복 불가		
 		System.out.println("나무를 도끼로" + b + "찍었다");
 		System.out.println("나무가 넘어가지 않는다");
-		System.out.println("나무를 도끼로"  + b + "더 찍었다");
+		System.out.println("나무를 도끼로"  + c + "더 찍었다");
 		System.out.println("나무가 넘어간다");
 		System.out.print("남아있는 나무는?");
 		System.out.println(a); // println 은 출력하고 줄바꿈도 포함 
