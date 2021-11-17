12c290f57b729785285157cb9ab0a0380446b057
조재웅 <@DESKTOP-P39DUQ7>
2021-11-17 13:48:04 +0900

    과자

--- src/hello2/Hello2Class.java
+++ src/hello2/Hello2Class.java
@@ -14,7 +14,7 @@ public class Hello2Class {
 		String b ="1회"; // 문자열 변수 선언
 		String c ="2회";
 		
-		int tree = 5; // 나무의 갯수
+		int snacks = 1000; // 나무의 갯수
 		
 		//나무를 넘길 수 있는 횟수
 		//1번 나무는 2번 
@@ -23,14 +23,14 @@ public class Hello2Class {
 		//4번 나무는 5번
 		//5번 나무는 10번
 		
-		System.out.println("나무가 " + tree + "그루가 있다");
+		System.out.println("나무가 " + snacks + "그루가 있다");
 		System.out.println("나무를 도끼로" + 1 + "회 찍었다");
 		System.out.println("나무가 넘어가지 않는다");
 		System.out.println("1번 나무를 도끼로"  + 2 + "회 찍었다");
 		System.out.println("1번 나무가 넘어간다");
 		System.out.print("남아있는 나무는? ");
-		tree = tree - 1;
-		System.out.println(tree); 
+		snacks = snacks - 1;
+		System.out.println(snacks); 
 		
 				
 		for (int i = 0; i < 10; i++) {
@@ -50,8 +50,27 @@ public class Hello2Class {
 				
 		}
 		System.out.print("남아있는 나무는? ");
-		System.out.println(tree); 
+		System.out.println(snacks); 
+		
+		for (int i = 0; i <0; i++) {
+			System.out.println(" 100원 짜리 과자를  " + (i+1) + "개 사먹었다.");
+			
+			if (i == 9) {     // 만약에 10번이 찍혔으면
+				
+				System.out.println("수중에 돈이없다."); // 나무가 넘어간다를 출력
+			
+			} else {   // 10번이 아니면
+				
+				System.out.println("돈이 남아있다. ");  // 나무가 넘어가지 않는다를 출력
+			snacks = snacks - 100;
+			
+			System.out.print("남아있는 돈은? : ");
+			System.out.println(snacks);
+		}
+		
+		
 
  }
 	
+}
 }
